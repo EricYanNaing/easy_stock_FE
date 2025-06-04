519b97721ebe8ca5e298f71071c1274e394b16d8
ericthedev7 <ericthedev97@gmail.com>
2025-06-04 17:56:44 +0700

    Update http.js

--- src/utils/http.js
+++ src/utils/http.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const http = axios.create({
-    baseURL : import.meta.env.VITE_API_BASE_URL || 'http://localhost:3000',
+    baseURL : import.meta.env.VITE_BASE_URL || 'http://localhost:3000',
     timeout : 10000
 })
 
